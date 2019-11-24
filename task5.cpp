ca3011c8340d0b5db82cb01d22cd3cce319030ca
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 22:32:02 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, m;
+	cout << "Введите размер матрицы (M * N): ";
+	cin >> m >> n;
+
+	int **a = new int*[m];
+
+	for (int i = 0; i < m; i++)
+	{
+		a[i] = new int[n];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Введите элементы " << i + 1 << " строки" << endl;
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Введите элементы: ";
+			cin >> a[i][j];
+		}
+	}
+	bool percent = false;
+	int column = 0;
+	for (int i = 0; i < n; i++)
+	{
+		percent = false;
+		for (int j = 0; j < m; j++)
+		{
+			if (a[j][i] % 2 == 1)
+			{
+				percent = true;
+				break;
+			}
+		}
+		if (percent == true)
+		{
+			column = i + 1;
+			break;;
+		}
+	}
+
+	cout << "Столбец: " << column << endl;
+
+	system("pause");
+	return 0;
+}
