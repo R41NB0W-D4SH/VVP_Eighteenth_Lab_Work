a491213600018c3eb02799d7e654aa434df17aaf
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 22:14:30 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, m;
+
+	cout << "Введите размер матрицы (M * N): ";
+	cin >> m >> n;
+
+	int **a = new int*[m];
+
+	for (int i = 0; i < m; i++)
+	{ 
+		a[i] = new int[n];
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Введите элементы " << i + 1 << " строки" << endl;
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Введите элемент: ";
+			cin >> a[i][j];
+		}
+	}
+
+	int *b = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		b[i] = 1;
+		for (int j = 0; j < m; j++)
+		{
+			b[i] *= a[j][i];
+		}
+	}
+
+	int min_i = 0;
+
+	for (int i = 1; i < n; i++)
+	{
+		min_i = b[min_i] > b[i] ? i : min_i;
+	}
+
+	cout << "Минимал: " << min_i + 1 << endl << "Mинимал произведение: " << b[min_i] << endl;
+
+	system("pause");
+	return 0;
+}
